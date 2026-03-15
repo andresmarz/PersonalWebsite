9c07d0f295c713d3407c9ca2b8080e7b6e6bd097
Andrés Márquez Zuleta <andresmarquezzuleta@gmail.com>
2026-03-14 22:03:05 -0400

    Create contact.js
    Adding Contact.js file on assets, js folder for communication with the Back End

---
+++ assets/js/contact.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.querySelector("#five form");
+
+    form.addEventListener("submit", async function (e) {
+
+        e.preventDefault();
+
+        const data = {
+            nombres: document.getElementById("nombre").value,
+            apellidos: document.getElementById("apellidos").value,
+            email: document.getElementById("email").value,
+            telefono: document.getElementById("telefono").value,
+            mensaje: document.getElementById("mensaje").value
+        };
+
+        try {
+
+            const response = await fetch("https://localhost:7202/api/contact", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (response.ok) {
+                alert("Mensaje enviado correctamente");
+                form.reset();
+            } else {
+                alert("Error al enviar el mensaje");
+            }
+
+        } catch (error) {
+            console.error(error);
+            alert("No se pudo conectar con el servidor");
+        }
+
+    });
+
+});
